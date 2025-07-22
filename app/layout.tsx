b79b2b2a240183bfae43eabf1dd83108530993f7
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2025-07-22 05:30:07 +0100

    This is the Noble scheduler

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Calendra",
-  description: "Calendra is a simple and efficient calendar app that helps you manage your events, meetings, and schedules with ease. Stay organized and never miss an important date again!",
+  title: "Noble Scheduler",
+  description: "noble scheduler is a simple and efficient calendar app that helps you manage your events, meetings, and schedules with ease. Stay organized and never miss an important date again!",
 };
 
 export default function RootLayout({
